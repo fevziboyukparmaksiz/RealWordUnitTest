95097cf536dffe7f4fa5229943bb45f38b899e3c
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-09-25 23:15:30 +0300

    Add project files.

---
+++ RealWordUnitTest.Web/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RealWordUnitTest.Web.Models;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public decimal? Price { get; set; }
+
+    public int? Stock { get; set; }
+
+    public string? Color { get; set; }
+}
