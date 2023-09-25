4cba71dbf8c0cc4fb3e01a3a301ed01aa2123fbf
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-09-26 00:11:59 +0300

    Unit Test Projemizin oluşturulması

---
+++ RealWordUnitTest.Test/ProductControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using RealWordUnitTest.Web.Controllers;
+using RealWordUnitTest.Web.Models;
+using RealWordUnitTest.Web.Repository;
+
+namespace RealWordUnitTest.Test
+{
+    public class ProductControllerTest
+    {
+        private readonly IMock<IRepository<Product>> _mockRepository;
+        private readonly ProductsController _productsController;
+        private List<Product> _products;
+        public ProductControllerTest()
+        {
+            _mockRepository = new Mock<IRepository<Product>>();
+            _productsController = new ProductsController(_mockRepository.Object);
+            _products = new List<Product>()
+            {
+                new() { Id = 1, Name = "Kalem", Price = 100, Stock = 50, Color = "Kırmızı" } ,
+                new() { Id = 2, Name = "Kitap", Price = 200, Stock = 40, Color = "Mavi" }
+            };
+        }
+
+    }
+}
